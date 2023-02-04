9ed246f74c9e8e30d7877e38b411e83b57e985f8
codemilo-ui <miloismypetdog@gmail.com>
2023-02-04 12:03:27 +0530

    Bug fix

--- cogs/Information.py
+++ cogs/Information.py
@@ -54,33 +54,18 @@ class Information(commands.Cog):
     @slash_command(name="invite", description="Invite CB42 to your server")
     @cooldown(1, 5, BucketType.user)
     async def invite(self, ctx):
-        invitebt = discord.Button(
-            label="Invite CB42",
-            url="https://discord.com/api/oauth2/authorize?client_id=1004727274031038574&permissions=8&redirect_uri=https%3A%2F%2Fcb42bot.tk&response_type=code&scope=bot%20connections%20applications.commands"
-        )
-        view = discord.View()
-        view.add_item(invitebt)
-
         embed = discord.Embed(title=f"Invite {self.client.user.name}",
                               description=f"Invite CB42 from [here](https://discord.com/api/oauth2/authorize?client_id=1004727274031038574&permissions=8&redirect_uri=https%3A%2F%2Fcb42bot.tk&response_type=code&scope=bot%20connections%20applications.commands)")
 
-        await ctx.respond(embed=embed, view=view, ephemeral=True)
+        await ctx.respond(embed=embed, ephemeral=True)
 
     @slash_command(name="credits", description="Shows who made CB42")
     @cooldown(1, 5, BucketType.user)
     async def credits(self, ctx):
-        cbt_button = discord.Button(
-            label="Github",
-            url="https://github.com/CB42Bot/CB42"
-        )
-        view = discord.View()
-        view.add_item(cbt_button)
-
         embed = discord.Embed(
-            title=f"Developers of {self.bot.user.name}",
-            description=f"CB42 was made by [sudo-adrian](https://github.com/sudo-adrian) and [codemilo-ui](https://github.com/codemilo-ui)"
+            title=f"Developers of [CB42](https://github.com/codemilo-ui/CB42)",
+            description=f"CB42 was made by [adzsx](https://github.com/adzsx) and [codemilo-ui](https://github.com/codemilo-ui)"
         )
-
         await ctx.respond(embed=embed, view=view)
 
 
